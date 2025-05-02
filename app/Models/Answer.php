a93afa0f1f6913c63025abfd6d8b3a8997dbc154
irikura <irikura.emerald@gmail.com>
2025-05-02 14:01:23 +0900

    クラスファイルの作成、リレーション設定

---
+++ app/Models/Answer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'question_id',
+        'answerer_id',
+        'message'
+    ];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'mail_address',
+        'telephonenumber',
+        'message'
+    ];
+
+    public function answer()
+    {
+        return $this->hasOne(Answer::class);
+    }
+}

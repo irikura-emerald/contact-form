064a8402f3456043873cfc0e139454652443db7f
irikura <irikura.emerald@gmail.com>
2025-05-02 15:48:50 +0900

    ファクトリー設定

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'mail_address' => fake()->email(),
+            'telephonenumber' => fake()->phoneNumber(),
+            'message' => fake()->realText(200)
+        ];
+    }
+}

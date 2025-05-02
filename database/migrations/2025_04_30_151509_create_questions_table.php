8a216d481ae18b3cdb0c352edd5a446312f1f5a3
irikura <irikura.emerald@gmail.com>
2025-05-02 14:00:27 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2025_04_30_151509_create_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name');
+            $table->string('mail_address')->nullable();
+            $table->string('telephonenumber')->nullable();
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};

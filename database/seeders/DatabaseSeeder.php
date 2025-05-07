1f2fd8f9745f1bc57c570f054972f16f2d59b574
irikura <irikura.emerald@gmail.com>
2025-05-07 10:38:50 +0900

    テストデータの作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Answer;
+use App\Models\Question;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // 未回答の問い合わせ
+        Question::factory(10)->create();
+
+        // ユーザ(回答者5人が10件の問い合わせに対応)
+        User::factory(5)->has(Answer::factory()->count(10))->create();
     }
 }

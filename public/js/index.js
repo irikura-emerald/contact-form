16b4923df698eb7809dae8a65cb962859681f5dc
irikura <irikura.emerald@gmail.com>
2025-05-16 11:00:44 +0900

    コメントを追加

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,14 @@
 window.onload = function () {
+    // 質問IDのテキストボックス
     let questionId = document.getElementById("question_id");
+
+    // 「回答を見る」ボタン
     let button = document.getElementById("show_button");
+
+    // 質問IDのセレクトボックス
     let select = document.getElementById("question_select");
 
+    // ボタンが押されたら、質問IDのページに遷移
     button.onclick = () => {
         let id = parseInt(questionId.value);
         if (id && Number.isInteger(id) && id > 0) {
@@ -10,6 +16,7 @@ window.onload = function () {
         }
     };
 
+    // セレクトボックスで質問IDを選択した場合、テキストボックスを無効にする
     select.onchange = () => {
         let id = parseInt(select.value);
         questionId.value = id != -1 ? id : "";
